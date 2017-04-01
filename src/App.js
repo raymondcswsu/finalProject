0a2ad65ec432d3693945878ef82b6a212149c299
Raymond Anderson <Raymond Anderson>
2017-04-01 12:22:19 -0600

    adding Result.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyForm from './MyForm.js'; 
+import Result from './Result.js'; 
 
-//<img src={baseballImg} alt="" /> 
+
 class App extends Component {
 	constructor(props)
 	{
 		  super(props);
 		  this.displayResults = this.displayResults.bind(this); 
+		  this.changeResults = this.changeResults.bind(this); 
+	}
+	
+	changeResults(results)
+	{
+		var temp =
+		{
+			schoolName : results.school.name, 
+			inStateTuit : results['2014'].cost.tuition.in_state, 
+			outStateTuit : results['2014'].cost.tuition.out_of_state,
+			medianEarnings6Yrs : results['2009'].earnings['6_yrs_after_entry'].median 
+			
+			
+		}
+		return temp; 
 	}
 	
 	
 	displayResults(toDisplay)
 	{
-		console.log(toDisplay); 
+		var results = toDisplay.results; 
+		//console.log(results); 
+		var results = results.map(this.changeResults);
+		console.log(results); 
+		
+		
+		
 	}
 	
   render() {
--- src/Result.js
+++ src/Result.js
@@ -6,18 +6,38 @@ class Result extends React.Component{
 	{
 		super(props); 
 		this.getInState = this.getInState.bind(this); 
+		this.getOutState = this.getOutState.bind(this); 
+		this.getSchoolName = this.getSchoolName.bind(this); 
+		this.getMedianEarn = this.getMedianEarn.bind(this); 
 	}
 	
 	getInState()
 	{
-		return this.props; 
+		return this.props.inStateTuit; 
+	}
+	
+	getOutState()
+	{
+		return this.props.outStateTuit;
+	}
+	
+	getSchoolName()
+	{
+		return this.props.schoolName; 
+	}
+	
+	getMedianEarn()
+	{
+		return this.props.medianEarnings6Yrs; 
 	}
 	
 	render(){
 		return(
 			<div> 
-				<p>Tutition(in-state): {this.getInState}</p>
-				<p> </p> 
+				<p>School Name: { this.getSchoolName}</p>  <br/>
+				<p>Tutition(in-state): {this.getInState}</p> <br/> 
+				<p>Tutition(out-state): {this.getOutState} </p>  <br/>
+				<p>Median Earnings After 6 Years: {this.getMedianEarn} </p>  <br/>
 			</div>
 		)
 	}
